bcd7315d6ad87d286cc82e9d74739cabd918c066
Samanja Cartagena <c.samanja09@Pabs.local>
2025-06-25 23:23:42 -0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+<h1>{{this.aegis}}</h1>
+</template>
+<script>
+import axios from 'axios'
+
+export default {
+    props:{},
+    components:{},
+    data(){
+     return {
+        aegis:null
+     }
+ 
+    },
+
+
+mounted(){
+ 
+ axios
+      .get('https://pokeapi.co/api/v2/pokemon-species/aegislash')
+      .then(response=> {console.log(this.aegis=response.data.base_happiness)})
+
+
+  
+}
+
+}
+</script>
+
+<style>
+
+</style>
